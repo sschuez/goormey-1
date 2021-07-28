ae90db2e1ddeaa0298a221467eea19804bb9005a
Stephen Schuez <stephen.schuez@gmail.com>
2021-07-28 16:16:59 +0200

    implemented nested associations, incl. delete. Also added basic lewagon footer to page

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -20,4 +20,20 @@ export default class extends Controller {
     var content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
     this.linksTarget.insertAdjacentHTML('beforebegin', content);
   }
+
+  remove_association(event) {
+    event.preventDefault()
+
+    let wrapper = event.target.closest(".nested-fields");
+
+    // New records are simply removed from the page
+    if (wrapper.dataset.newRecord == "true") {
+      wrapper.remove();
+
+    // Existing records are hidden and flagged for deletion
+    } else {
+      wrapper.querySelector("input[name*='_destroy']").value = 1;
+      wrapper.style.display = 'none';
+    }
+  }
 }
