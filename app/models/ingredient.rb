830cc109a103fc1919348fa2cc85ac7f2acf3589
Stephen Schuez <stephen.schuez@gmail.com>
2021-07-29 17:01:27 +0200

    added basic design to show page for a recipe

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,14 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
+
+  MEASUREMENTS = {
+    "g" => "gram",
+    "kg" => "kilo",
+    "tbs" => "table spoon",
+    "tsp" => "tea spoon",
+    "l" => "litre",
+    "ml" => "millilitre"
+  }
+
+  
 end
