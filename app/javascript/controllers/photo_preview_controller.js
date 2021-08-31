bf36f49311820c071bf63fe048338734668bf478
Stephen Schuez <stephen.schuez@gmail.com>
2021-08-31 15:30:49 +0200

    implemented photo upload button and added small preview image with stimulus

--- app/javascript/controllers/photo_preview_controller.js
+++ app/javascript/controllers/photo_preview_controller.js
@@ -1,12 +1,29 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-
-  // connect() {
-  //   console.log("hello");
-  // }
+  static targets = [ "photoInput", "photoPreview" ]
 
   photo_input() {
-    console.log("hi")
+    const input = this.photoInputTarget
+
+    if (input) {
+      // add a listener to know when a new picture is uploaded
+      input.addEventListener('change', () => {
+        // call the displayPreview function, which retrieves the image url and displays it)
+        displayPreview(input);
+      })
+    }
+
+    const displayPreview = (input) => {
+      if (input.files && input.files[0]) {
+        const reader = new FileReader()
+        reader.onload = (event) => {
+          this.photoPreviewTarget.src = event.currentTarget.result
+        }
+        reader.readAsDataURL(input.files[0])
+        this.photoPreviewTarget.classList.remove('hidden')
+      }
+    }
   }
+
 }
