89f8dc0e1688cada1c45ec862b51b6c08f7bb0cc
Stephen Schuez <stephen.schuez@gmail.com>
2021-10-29 17:05:57 +0200

    changed seedfiles according to latest schema

--- db/seeds/02_recipes.rb
+++ db/seeds/02_recipes.rb
@@ -7,7 +7,9 @@ RECIPES_TO_CREATE.times do
   Recipe.create(
     user_id: User.ids.sample,
     name: Faker::Food.dish,
-    description: Faker::Food.description
+    description: Faker::Food.description,
+    serves: rand(1..5),
+    wizard_complete: true
   )
   progressbar.increment
 end
--- db/seeds/03_ingredients.rb
+++ db/seeds/03_ingredients.rb
@@ -3,17 +3,10 @@ progressbar = ProgressBar.create(
   total: Recipe.count * INGREDIENTS_ON_EACH_RECIPE
 )
 
-measurements = []
-Ingredient::MEASUREMENTS.each do |k,v|
-  measurements << k
-end
-
 Recipe.all.each do |recipe|
   INGREDIENTS_ON_EACH_RECIPE.times do
     recipe.ingredients.create(
-      amount: rand(1..100),
-      measurement: measurements.sample,
-      name: Faker::Food.ingredient
+      description: Faker::Food.ingredient
       )
     progressbar.increment
   end
