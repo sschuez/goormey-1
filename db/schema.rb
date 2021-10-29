4414f585b9971c7c78863391118b5ee93398ee6e
Stephen Schuez <stephen.schuez@gmail.com>
2021-10-29 16:08:58 +0200

    added 'wizard_complete' to recipe model to create scope of only finished wizards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_29_122229) do
+ActiveRecord::Schema.define(version: 2021_10_29_140402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2021_10_29_122229) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.integer "serves"
+    t.boolean "wizard_complete"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
