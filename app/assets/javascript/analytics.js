1c8cb96bd73e713c9db62cea21502cf097359784
Stephen Schuez <stephen.schuez@gmail.com>
2021-12-13 20:04:45 +0100

    added google tag manager instead of analytics

--- app/assets/javascript/analytics.js
+++ app/assets/javascript/analytics.js
@@ -1,12 +1,14 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-
-gtag('js', new Date());
+window.dataLayer = window.dataLayer || []
+function gtag() { dataLayer.push(arguments) }
 
 const trackGoogleAnalytics = (event) => {
-  gtag('config', 'G-FH88M0RRG5', {
-    'cookie_flags': 'max-age=7200;secure;samesite=none'
-  })
+
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-MCMNLVP');
+
 }
 
 document.addEventListener('turbolinks:load', trackGoogleAnalytics)
