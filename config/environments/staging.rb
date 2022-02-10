4e90c4997ee2c2c4f605c48d5c1f62269dd35125
Stephen Schuez <stephen.schuez@gmail.com>
2022-02-10 16:38:23 +0100

    adedd default url to staging environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "goormey_1_production"
 
+  
+  config.action_mailer.default_url_options = { :host => 'www.goormey-1-staging.herokuapp.com' }
   config.action_mailer.perform_caching = false
   
   config.action_mailer.delivery_method = :smtp
