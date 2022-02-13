ed2ab9e71849ddc74555bb21e9cea406781ee0c7
Stephen Schuez <stephen.schuez@gmail.com>
2022-02-13 22:58:21 +0100

    added ujs

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,10 @@
 import { Application } from "@hotwired/stimulus"
 import TextareaAutogrow from "stimulus-textarea-autogrow"
+import Rails from "@rails/ujs"
 
 const application = Application.start()
 application.register("textarea-autogrow", TextareaAutogrow)
+Rails.start()
 
 // Configure Stimulus development experience
 application.debug = false
