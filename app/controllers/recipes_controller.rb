6284a13488e205315b32a9d750b84927780d879a
Stephen Schuez <stephen.schuez@gmail.com>
2022-02-14 00:59:40 +0100

    changed url to path

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -52,7 +52,7 @@ class RecipesController < ApplicationController
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to @recipe, notice: "Recipe was successfully updated." }
+        format.html { redirect_to recipe_path(@recipe), notice: "Recipe was successfully updated." }
         format.json { render :show, status: :ok, location: @recipe }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -64,7 +64,7 @@ class RecipesController < ApplicationController
   # DELETE /recipes/1 or /recipes/1.json
   def destroy
     @recipe.destroy
-    redirect_to root_url, status: :see_other, notice: "Recipe was successfully deleted."
+    redirect_to root_path, status: :see_other, notice: "Recipe was successfully deleted."
     # respond_to do |format|
     #   format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
     #   format.json { head :no_content }
