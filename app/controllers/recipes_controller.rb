7d4d6fb7e56dcc170ef03ba5d41c53543ec8b4e1
Stephen Schuez <stephen.schuez@gmail.com>
2022-02-13 22:16:47 +0100

    remove turbo from all forms

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -64,7 +64,7 @@ class RecipesController < ApplicationController
   # DELETE /recipes/1 or /recipes/1.json
   def destroy
     @recipe.destroy
-    redirect_to root_path, status: :see_other, notice: "Recipe was successfully deleted."
+    redirect_to root_url, status: :see_other, notice: "Recipe was successfully deleted."
     # respond_to do |format|
     #   format.html { redirect_to recipes_url, notice: "Recipe was successfully destroyed." }
     #   format.json { head :no_content }
