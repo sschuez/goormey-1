dd231e8a3ea23888680f562fae4c00c834aa31ca
Stephen Schuez <stephen.schuez@gmail.com>
2022-03-10 17:19:36 +0100

    Add validation to comment

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import CommentsCheckController from "./comments_check_controller.js"
+application.register("comments-check", CommentsCheckController)
+
 import DragController from "./drag_controller.js"
 application.register("drag", DragController)
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,26 @@
 class Comment < ApplicationRecord
+  #Organisation Model (Shortcut: mstr)
+  # == Constants ============================================================
+  
+  # == Attributes ===========================================================
+  
+  # == Extensions ===========================================================
+  
+  # == Relationships ========================================================
   belongs_to :recipe
   belongs_to :user
 
   broadcasts_to :recipe, inserts_by: :prepend
+  
+  # == Validations ==========================================================
+  validates :content, presence: true, length: { minimum: 1, maximum: 200}
 
+  # == Scopes ===============================================================
+  
+  # == Callbacks ============================================================
+  
+  # == Class Methods ========================================================
+  
+  # == Instance Methods =====================================================
+  
 end
