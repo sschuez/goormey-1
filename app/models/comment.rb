0e8a3e0775c208932c7c823df83d7897f4a5a205
Stephen Schuez <stephen.schuez@gmail.com>
2022-03-10 09:17:03 +0100

    Fix broadcasting to recipe

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
 
-  # broadcasts_to :recipe
-
-  after_create_commit do
-    broadcast_prepend_to :recipe,
-      target: "comments",
-      partial: "comments/comment",
-      locals: { comment: self }
-  end
+  broadcasts_to :recipe, inserts_by: :prepend
 
 end
