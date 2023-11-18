49ff8ce316d85d8a37e5adc780d51461eeecd1ef
Stephen Schuez <stephens@hey.com>
2023-11-18 10:18:36 +0100

    change redis initialize

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,9 @@
-$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+redis_config = Rails.env.development? ? { url: "redis://localhost:6379/1" } : { url: ENV["REDIS_URL"] }
+
+Sidekiq.configure_server do |config|
+  config.redis = redis_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_config
+end
