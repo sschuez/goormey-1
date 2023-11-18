be79e677ca4ff5648b2e9d56bed6ce207e297ee9
Stephen Schuez <stephens@hey.com>
2023-11-18 10:19:41 +0100

    add guard clause in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+return unless Rails.env.development?
+
 USERS_TO_CREATE = 10
 RECIPES_TO_CREATE = 50
 INGREDIENTS_ON_EACH_RECIPE = 5
